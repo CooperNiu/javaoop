79c2cf3227e4eb31a8a790081acc15ae967b346b
CooperNiu <niuyanno.1@outlook.com>
2019-07-12 13:12:34 +0800

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import model.Student;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student student = new Student(111,"Danny",22,"male");
+        System.out.println(student);
+    }
+}
